4b4029e2b3fb9791ad66c04ad6d04b693a745a8d
Brittany <brittany.bolling@aol.com>
2023-04-24 11:29:47 +0200

    push

--- save_json.js
+++ save_json.js
@@ -1,17 +1,15 @@
 const fs = require("fs");
 const path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
-const save = (favNumber) => {
-  console.log("saving");
-  fs.writeFile(
-    path.join(__dirname, ".", "number.json"),
-    JSON.stringify(favNumber, null, 2),
-    (error) => {
-      if (error) {
-        throw error;
-      }
-    }
-  );
+const save = async (favNumber) => {
+    console.log("saving");
+    await s3.putObject({
+        Body: JSON.stringify(favNumber, null, 2),
+        Bucket: "cyclic-dull-puce-dhole-toga-us-east-1",
+        Key: "number.json",
+    }).promise()
 };
 
 module.exports = { save };
